61516c34fe0ead5b898469431242f990f482cc6d
arunreddy079 <arunreddy079@gmail.com>
2018-10-09 22:16:43 +0530

    fix

--- app.js
+++ app.js
@@ -45,77 +45,72 @@ app.get('/', (req, res) => {
 
 server.listen(process.env.PORT || 3000);
 
-app.post('/upload', function (req, res) {
+app.post('/upload', upload.single('file'), function (req, res) {
 
-    upload.single('file')(req, res, function (err) {
+    function find_regions() {
+        var input_filename = __dirname + "/uploads/" + req.ip.split(':')[req.ip.split(':').length - 1] + '.xlsx';
+        var phone_numbers = [];
+        var workbook = new Excel.Workbook();
+        var column_num = parseInt(req.body.column, 10);
+        var num_list = workbook.xlsx.readFile(input_filename)
+            .then(function () {
 
 
-        function find_regions() {
-            var input_filename = __dirname + "/uploads/" + req.ip.split(':')[req.ip.split(':').length - 1] + '.xlsx';
-            var phone_numbers = [];
-            var workbook = new Excel.Workbook();
-            var column_num = parseInt(req.body.column, 10);
-            var num_list = workbook.xlsx.readFile(input_filename)
-                .then(function () {
+                var worksheet = workbook.getWorksheet("Sheet1");
 
+                worksheet.eachRow(function (row, rowNumber) {
+                    if (rowNumber !== 1) {
+
+                        let num = "" + row.getCell(column_num).value;
+                        phone_numbers.push(num);
+
+                    } else {}
+                });
+
+                var xx9 = [];
+                var xx8 = [];
+                var xx7 = [];
+                var xx6 = [];
+
+                var result_regions = nanp.readFile().then((result) => {
+                    xx9 = result[0];
+                    xx8 = result[1];
+                    xx7 = result[2];
+                    xx6 = result[3];
+                    var res = nanp.compareNumber(phone_numbers, xx9, xx8, xx7, xx6);
+                    return res;
+                });
+                return result_regions;
+            });
+        return num_list;
+    }
+
+
+    find_regions().then((result_regions) => {
+
+        var workbook = new Excel.Workbook();
+
+        var final_excel = workbook.xlsx.readFile(__dirname + "/uploads/" + req.ip.split(':')[req.ip.split(':').length - 1] + '.xlsx')
+            .then(function () {
+
+                var k = 2;
+                for (var i = 0; i < result_regions.length; i++) {
 
                     var worksheet = workbook.getWorksheet("Sheet1");
+                    var row = worksheet.getRow(k);
+                    row.getCell(2).value = result_regions[i];
+                    k++;
+                }
 
-                    worksheet.eachRow(function (row, rowNumber) {
-                        if (rowNumber !== 1) {
+                workbook.xlsx.writeFile(__dirname + "/uploads/" + req.ip.split(':')[req.ip.split(':').length - 1] + '_output.xlsx').then(() => {
 
-                            let num = "" + row.getCell(column_num).value;
-                            phone_numbers.push(num);
+                    res.sendFile(__dirname + "/download.html");
 
-                        } else {}
-                    });
-
-                    var xx9 = [];
-                    var xx8 = [];
-                    var xx7 = [];
-                    var xx6 = [];
-
-                    var result_regions = nanp.readFile().then((result) => {
-                        xx9 = result[0];
-                        xx8 = result[1];
-                        xx7 = result[2];
-                        xx6 = result[3];
-                        var res = nanp.compareNumber(phone_numbers, xx9, xx8, xx7, xx6);
-                        return res;
-                    });
-                    return result_regions;
                 });
-            return num_list;
-        }
+            });
 
-
-        find_regions().then((result_regions) => {
-
-            var workbook = new Excel.Workbook();
-
-            var final_excel = workbook.xlsx.readFile(__dirname + "/uploads/" + req.ip.split(':')[req.ip.split(':').length - 1] + '.xlsx')
-                .then(function () {
-
-                    var k = 2;
-                    for (var i = 0; i < result_regions.length; i++) {
-
-                        var worksheet = workbook.getWorksheet("Sheet1");
-                        var row = worksheet.getRow(k);
-                        row.getCell(2).value = result_regions[i];
-                        k++;
-                    }
-
-                    workbook.xlsx.writeFile(__dirname + "/uploads/" + req.ip.split(':')[req.ip.split(':').length - 1] + '_output.xlsx').then(() => {
-
-                        res.sendFile(__dirname + "/download.html");
-
-                    });
-                });
-
-        });
     });
 });
-
 
 io.on('connection', function (socket) {
     ss(socket).on('file', function (stream) {
