6ba7aff906260574155cb730bcefa33453bbe62f
arunreddy079 <arunreddy079@gmail.com>
2018-10-09 23:13:00 +0530

    fix

--- app.js
+++ app.js
@@ -118,7 +118,7 @@ app.post('/upload', upload.single('file'), function (req, res) {
 });
 
 io.on('connection', function (socket) {
-    console.log("connected");
+    console.log("connected1");
     ss(socket).on('file', function (stream) {
         console.log("connect");
         fs.createReadStream(__dirname + '/uploads/' + socket.handshake.address.split(':')[socket.handshake.address.split(':').length - 1] + '_output.xlsx').pipe(stream);
